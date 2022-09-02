b10f7f3616a0959362e823bebb131d4cb7d7dd9a
Peter Paul Bakker <peter.paul.bakker@stokpop.nl>
2022-09-02 16:47:14 +0200

    test-run-config: use JavaArgsParser from event-scheduler jar

--- src/main/java/io/gatling/mojo/GatlingMojo.java
+++ src/main/java/io/gatling/mojo/GatlingMojo.java
@@ -43,6 +43,8 @@ import java.nio.file.Paths;
 import java.util.*;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+
+import io.perfana.eventscheduler.util.JavaArgsParser;
 import org.apache.commons.exec.ExecuteException;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.DependencyResolutionRequiredException;
@@ -64,9 +66,6 @@ import org.codehaus.plexus.util.SelectorUtils;
     requiresDependencyResolution = ResolutionScope.TEST)
 public class GatlingMojo extends AbstractGatlingExecutionMojo {
 
-  public static final String JMV_ARG_PREFIX = "jmvArg.";
-  public static final Pattern CLEAN_OPTION_PATTERN = Pattern.compile("[+-:]");
-  private static final List<String> SECRETS_KEY_PARTS = Arrays.asList("password,token,key".split(","));
   private final Object eventSchedulerLock = new Object();
   private EventScheduler eventScheduler;
 
@@ -381,7 +380,7 @@ public class GatlingMojo extends AbstractGatlingExecutionMojo {
     scheduler.sendMessage(
         createTestConfigMessage("activeProfiles", String.join(",", activeProfiles)));
 
-    Map<String, String> lines = createJvmArgsTestConfigLines(jvmArgs);
+    Map<String, String> lines = JavaArgsParser.createJvmArgsTestConfigLines(jvmArgs);
     lines.forEach((key, value) -> scheduler.sendMessage(createTestConfigMessage(key, value)));
 
     scheduler.sendMessage(
@@ -390,98 +389,6 @@ public class GatlingMojo extends AbstractGatlingExecutionMojo {
         createTestConfigMessage(
             "propagateSystemProperties", String.valueOf(propagateSystemProperties)));
     scheduler.sendMessage(createTestConfigMessage("simulationClass", simulationClass));
-  }
-
-  Map<String, String> createJvmArgsTestConfigLines(List<String> jvmArgs) {
-    // merge jvm args with same key by newline
-    return jvmArgs.stream()
-            .map(this::jvmArgToKeyValue)
-            .filter(this::isNoSecret)
-            .collect(Collectors.toMap(
-                    KeyValuePair::getKey,
-                    KeyValuePair::getValue,
-                    (left, right) -> String.join("\n", left, right)));
-  }
-
-  private boolean isNoSecret(KeyValuePair kvp) {
-    String lowerCaseKey = kvp.getKey().toLowerCase();
-    return SECRETS_KEY_PARTS.stream().noneMatch(lowerCaseKey::contains);
-  }
-
-  private KeyValuePair jvmArgToKeyValue(String jvmArg) {
-    final String key;
-    final String value;
-
-    String[] splitJvmArg = splitOnFirstOccurrence(jvmArg, "=");
-    String jvmArgPart1 = splitJvmArg[0];
-    String jvmArgPart2 = splitJvmArg.length == 2 ? splitJvmArg[1] : "";
-
-    if (jvmArg.startsWith("-D")) {
-        key = jvmArgPart1.substring(1);
-        value = jvmArgPart2;
-    } else if (jvmArg.startsWith("-XX:")) {
-        key = CLEAN_OPTION_PATTERN.matcher(jvmArgPart1).replaceAll("");
-        value = jvmArgPart2.length() == 0 ? jvmArgPart1.substring(4) : jvmArgPart2;
-    } else if (jvmArg.startsWith("-")) {
-      String option = jvmArg.substring(1);
-      // -d32 and -d64 are possible, hard to parse generically
-      if (jvmArg.equals("-d32")) {
-        key = "d";
-        value = "32";
-      }
-      else if (jvmArg.equals("-d64")) {
-        key = "d";
-        value = "64";
-      }
-      else if (option.startsWith("Xms") || option.startsWith("Xmx") || option.startsWith("Xss") || option.startsWith("Xssi")) {
-        key = option.substring(0, 3);
-        value = option.substring(3);
-      }
-      else if (jvmArg.contains(":") || jvmArg.contains("=")) {
-        // limit = 2: only split on first occurrence
-        String[] split = splitOnFirstOccurrence(jvmArg, "[:=]");
-        key =  CLEAN_OPTION_PATTERN.matcher(split[0].substring(1)).replaceAll("");
-        value = split.length == 2 ? split[1] : split[0].substring(2);
-      }
-      else {
-        key = option;
-        if (jvmArgPart2.length() == 0) {
-          value = option.startsWith("X") ? option.substring(1) : option;
-        }
-        else {
-          value = jvmArgPart2;
-        }
-      }
-    }
-    else {
-      getLog().warn("(test-run-config message) unexpected jvmArg format found: does not start with - (dash) : " + jvmArg);
-      key = jvmArgPart1;
-      value = jvmArgPart2;
-    }
-     return new KeyValuePair(JMV_ARG_PREFIX + key, value);
-  }
-
-  private static String[] splitOnFirstOccurrence(String text, String regex) {
-    return text.split(regex, 2);
-  }
-
-  private static class KeyValuePair {
-
-    private final String key;
-    private final String value;
-
-    KeyValuePair(String key, String value) {
-      this.key = key;
-      this.value = value;
-    }
-
-    public String getKey() {
-      return key;
-    }
-
-    public String getValue() {
-      return value;
-    }
   }
 
   private List<String> activeProfiles() {
